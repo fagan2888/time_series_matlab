9810497cc7dfb5f8cbc92a4ffb22ced8b7cb6967
Andrea Pasqualini <apsql@users.noreply.github.com>
2018-04-10 01:30:24 +0200

    Added variance estimation

--- var_lib/ols.m
+++ var_lib/ols.m
@@ -1,5 +1,24 @@
-function b = ols(y, x)
+function [b, s2] = ols(y, x)
 
-b = x' * x \ x' * y;
+% Assuming first dimension (rows) run along time indices.
+% Assuming second dimension (columns) run along variable indices.
+% Assuming contiguous time series (no NaNs in the middle of the series).
+% Assuming contiguous lags, i.e., j \in {1, 2, 3, ..., p}.
+
+T = size(x, 1);
+N = size(x, 2);
+
+if size(y, 1) ~= T
+    error('LHS does not have equal amount of obs. as RHS.')
+end
+
+ixx = (x' * x) \ eye(N);
+
+b = ixx * (x' * y);
+
+if nargout > 1
+    resid = y - x * b;
+    s2 = (resid' * resid) ./ (T-N);
+end
 
 end
